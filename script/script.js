c535c5306352a4250bcc7d2df7633a04ae67e3fe
oysteinsigholt <oysteinsigholt@users.noreply.github.com>
2017-07-27 13:21:04 +0200

    feat(openapi): initial openapi support

--- script/script.js
+++ script/script.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  Barba.Pjax.start();
-  Barba.Prefetch.init();
+    Barba.Pjax.start();
+    Barba.Prefetch.init();
+    Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container) {
+        var js = container.querySelector("script");
+        if(js != null){
+            eval(js.innerHTML);
+            if (typeof pageReady === "function") {
+                pageReady();
+            }
+        }
+    });
 });

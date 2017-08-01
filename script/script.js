774dffa26277b48b73835f946e89079119a4d1fd
oysteinsigholt <oysteinsigholt@users.noreply.github.com>
2017-08-01 11:45:24 +0200

    fix(openapi): respond to comments

--- script/script.js
+++ script/script.js
@@ -2,12 +2,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
     Barba.Pjax.start();
     Barba.Prefetch.init();
     Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container) {
-        var js = container.querySelector("script");
-        if(js != null){
-            eval(js.innerHTML);
-            if (typeof pageReady === "function") {
-                pageReady(container);
-            }
+        const js = container.querySelector("script");
+        if (js === null) {
+            return;
+        }
+
+        eval(js.innerHTML);
+        if (typeof pageReady === "function") {
+            pageReady(container);
         }
     });
 });
